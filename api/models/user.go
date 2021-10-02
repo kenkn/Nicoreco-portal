ab3bf54214b6f29264579b59aff3ac8ab5adc1ff
kenkn <skt7tp@gmail.com>
2021-10-02 18:07:23 +0900

    不要なUTF8宣言の消去

--- api/models/user.go
+++ api/models/user.go
@@ -10,9 +10,9 @@ import "gorm.io/gorm"
 // ユーザ情報
 type User struct {
 	gorm.Model
-	DisplayName string `json:"display_name" sql:"type:VARCHAR(30) CHARACTER SET utf8 COLLATE utf8_general_ci"`
+	DisplayName string `json:"display_name"`
 	UserID      string `json:"user_id" gorm"unique"`
-	Grade       string `json:"grade sql:"type:VARCHAR(30) CHARACTER SET utf8 COLLATE utf8_general_ci"`
+	Grade       string `json:"grade`
 	Email       string `json:"email" gorm:"unique"`
 	Password    []byte `json:"-"` // 非表示
 }
