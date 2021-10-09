3391099609e8255dbb2f92273f2470713312ab17
kenkn <skt7tp@gmail.com>
2021-10-09 17:56:37 +0900

    UIの微調整

--- ui/src/pages/LabReview.vue
+++ ui/src/pages/LabReview.vue
@@ -50,10 +50,6 @@
         </form>
       </div>
       <div class="p-4">
-        <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="currentColor" class="bi bi-file-plus mr-3" viewBox="0 0 16 16">
-          <path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H4.414A2 2 0 0 0 3 11.586l-2 2V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12.793a.5.5 0 0 0 .854.353l2.853-2.853A1 1 0 0 1 4.414 12H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
-          <path d="M3 3.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 6a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 6zm0 2.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z"/>
-        </svg>
         <h1 class="mt-3 display-6 d-inline">レビューする</h1>
         <form action="" @submit.prevent="submitReview" class="p-3">
           <router-link v-if="!auth" class="pageLink d-inline btn btn-outline-primary" to="/login">
