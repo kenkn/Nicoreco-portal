99be4d45df5b8ec8ba1e4ade913bd7f104f0e412
g063ff <zyagariko0001@icloud.com>
2021-10-11 17:05:05 +0900

    fix form and ui

--- ui/src/pages/CreateQuestion.vue
+++ ui/src/pages/CreateQuestion.vue
@@ -3,15 +3,26 @@
     <h1 class="pb-3 display-5">質問する</h1>
     <div class="p-3 border border-dark bg-white rounded shadow-sm">
       <h2 class="display-6">講義名:{{ Subject }}</h2>
-        <form action="" @submit.prevent="submit">
+        <!-- <form action="" @submit.prevent="submit"> -->
+          <!-- <div class="form-group"> -->
+            <!-- <span>タイトル:</span> -->
+            <!-- <input v-model="title" type="text" class="form-control w-100" required /> -->
+            <!-- <p class="m-0 py-2">本文:</p> -->
+            <!-- <textarea v-model="body" class="form-control p-1 my-2 mb-4" rows="4" required /> -->
+            <!-- <button class="btn btn-outline-primary w-100" type="submit">質問を送信</button> -->
+          <!-- </div> -->
+        <!-- </form> -->
+        <VeeForm @submit="submit" :validation-schema="schema">
           <div class="form-group">
             <span>タイトル:</span>
-            <input v-model="title" type="text" class="form-control w-100" required />
+            <VeeField name="title" v-model="title" class="form-control w-100" required />
+            <ErrorMessage name="title" />
             <p class="m-0 py-2">本文:</p>
-            <textarea v-model="body" class="form-control p-1 my-2 mb-4" rows="4" required />
-            <button class="btn btn-outline-primary w-100" type="submit">質問を送信</button>
+            <VeeField name="body" v-model="body" as="textarea" class="form-control p-1" rows="4" required />
+            <ErrorMessage name="body" />
+            <button class="btn btn-outline-primary w-100 mt-4" type="submit">質問を送信</button>
           </div>
-        </form>
+        </VeeForm>
 
     </div>
   </div>
@@ -21,9 +32,16 @@
 import { ref } from 'vue'
 import axios from 'axios'
 import { useRouter } from 'vue-router'
+import { Form as VeeForm, Field as VeeField, ErrorMessage } from 'vee-validate';
+import * as yup from 'yup';
 import subjectData from '../data/subject-data.json'
 export default {
   name: "CreateQuestion",
+  components: {
+    VeeForm,
+    VeeField,
+    ErrorMessage,
+  },
   data() {
     let code = ""
     let Subject = ""
@@ -37,9 +55,14 @@ export default {
         code = d.code
       }
     }
+    // バリデーション
+    const schema = yup.object({
+      title: yup.string().required("この項目は必須です").max(50, "タイトルは50文字以下で入力してください"),
+      body: yup.string().required("この項目は必須です"),
+    });
 
     const submit = async () => {
-      // 確認(いらんかったら消してください) 
+      // 確認
       if(window.confirm('送信します')) {
         await axios.post("/question/post", {
           questioner_id : localStorage.userID,
@@ -55,6 +78,7 @@ export default {
       Subject,
       title,
       body,
+      schema,
       submit
     }
   },
