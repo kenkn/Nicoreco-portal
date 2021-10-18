9542a42501140c97b4aec578d8c0db5dd6a810a9
kenkn <skt7tp@gmail.com>
2021-10-18 19:03:08 +0900

    LabLGTM機能の実装

--- ui/src/pages/LabReview.vue
+++ ui/src/pages/LabReview.vue
@@ -10,13 +10,21 @@
           <span class="text-secondary m-0 pl-3">レビュー日時: {{ review.CreatedAt }} </span>
           <div class="d-none lgtm-info">
             <!-- ここにlgtmボタンの分岐 -->
-            <div class="mt-1">
-              <button id="lgtm" class="btn btn-outline-primary lgtm">
+            <div v-if='!reviewLgtm[review.ID]' class="mt-1">
+              <button @click="updateReviewLgtm(review.ID)" id="lgtm" class="btn btn-outline-primary lgtm">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-hand-thumbs-up" viewBox="0 0 16 16">
                   <path d="M8.864.046C7.908-.193 7.02.53 6.956 1.466c-.072 1.051-.23 2.016-.428 2.59-.125.36-.479 1.013-1.04 1.639-.557.623-1.282 1.178-2.131 1.41C2.685 7.288 2 7.87 2 8.72v4.001c0 .845.682 1.464 1.448 1.545 1.07.114 1.564.415 2.068.723l.048.03c.272.165.578.348.97.484.397.136.861.217 1.466.217h3.5c.937 0 1.599-.477 1.934-1.064a1.86 1.86 0 0 0 .254-.912c0-.152-.023-.312-.077-.464.201-.263.38-.578.488-.901.11-.33.172-.762.004-1.149.069-.13.12-.269.159-.403.077-.27.113-.568.113-.857 0-.288-.036-.585-.113-.856a2.144 2.144 0 0 0-.138-.362 1.9 1.9 0 0 0 .234-1.734c-.206-.592-.682-1.1-1.2-1.272-.847-.282-1.803-.276-2.516-.211a9.84 9.84 0 0 0-.443.05 9.365 9.365 0 0 0-.062-4.509A1.38 1.38 0 0 0 9.125.111L8.864.046zM11.5 14.721H8c-.51 0-.863-.069-1.14-.164-.281-.097-.506-.228-.776-.393l-.04-.024c-.555-.339-1.198-.731-2.49-.868-.333-.036-.554-.29-.554-.55V8.72c0-.254.226-.543.62-.65 1.095-.3 1.977-.996 2.614-1.708.635-.71 1.064-1.475 1.238-1.978.243-.7.407-1.768.482-2.85.025-.362.36-.594.667-.518l.262.066c.16.04.258.143.288.255a8.34 8.34 0 0 1-.145 4.725.5.5 0 0 0 .595.644l.003-.001.014-.003.058-.014a8.908 8.908 0 0 1 1.036-.157c.663-.06 1.457-.054 2.11.164.175.058.45.3.57.65.107.308.087.67-.266 1.022l-.353.353.353.354c.043.043.105.141.154.315.048.167.075.37.075.581 0 .212-.027.414-.075.582-.05.174-.111.272-.154.315l-.353.353.353.354c.047.047.109.177.005.488a2.224 2.224 0 0 1-.505.805l-.353.353.353.354c.006.005.041.05.041.17a.866.866 0 0 1-.121.416c-.165.288-.503.56-1.066.56z"/>
                 </svg>
               </button>
-              <span class="pl-2">{{ review.lgtm }}件</span>
+              <span class="pl-2">{{ reviewLgtmCount[review.ID] }}件</span>
+            </div>
+            <div @click="updateReviewLgtm(review.ID)" v-else class="mt-1">
+              <button id="lgtm" class="btn btn-primary lgtm">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-hand-thumbs-up" viewBox="0 0 16 16">
+                  <path d="M8.864.046C7.908-.193 7.02.53 6.956 1.466c-.072 1.051-.23 2.016-.428 2.59-.125.36-.479 1.013-1.04 1.639-.557.623-1.282 1.178-2.131 1.41C2.685 7.288 2 7.87 2 8.72v4.001c0 .845.682 1.464 1.448 1.545 1.07.114 1.564.415 2.068.723l.048.03c.272.165.578.348.97.484.397.136.861.217 1.466.217h3.5c.937 0 1.599-.477 1.934-1.064a1.86 1.86 0 0 0 .254-.912c0-.152-.023-.312-.077-.464.201-.263.38-.578.488-.901.11-.33.172-.762.004-1.149.069-.13.12-.269.159-.403.077-.27.113-.568.113-.857 0-.288-.036-.585-.113-.856a2.144 2.144 0 0 0-.138-.362 1.9 1.9 0 0 0 .234-1.734c-.206-.592-.682-1.1-1.2-1.272-.847-.282-1.803-.276-2.516-.211a9.84 9.84 0 0 0-.443.05 9.365 9.365 0 0 0-.062-4.509A1.38 1.38 0 0 0 9.125.111L8.864.046zM11.5 14.721H8c-.51 0-.863-.069-1.14-.164-.281-.097-.506-.228-.776-.393l-.04-.024c-.555-.339-1.198-.731-2.49-.868-.333-.036-.554-.29-.554-.55V8.72c0-.254.226-.543.62-.65 1.095-.3 1.977-.996 2.614-1.708.635-.71 1.064-1.475 1.238-1.978.243-.7.407-1.768.482-2.85.025-.362.36-.594.667-.518l.262.066c.16.04.258.143.288.255a8.34 8.34 0 0 1-.145 4.725.5.5 0 0 0 .595.644l.003-.001.014-.003.058-.014a8.908 8.908 0 0 1 1.036-.157c.663-.06 1.457-.054 2.11.164.175.058.45.3.57.65.107.308.087.67-.266 1.022l-.353.353.353.354c.043.043.105.141.154.315.048.167.075.37.075.581 0 .212-.027.414-.075.582-.05.174-.111.272-.154.315l-.353.353.353.354c.047.047.109.177.005.488a2.224 2.224 0 0 1-.505.805l-.353.353.353.354c.006.005.041.05.041.17a.866.866 0 0 1-.121.416c-.165.288-.503.56-1.066.56z"/>
+                </svg>
+              </button>
+              <span class="pl-2">{{ reviewLgtmCount[review.ID] }}件</span>
             </div>
             <!-- ここまで -->
           </div>
@@ -77,13 +85,15 @@ import labData from '../data/lab-data.json'
 export default {
   name: "LabReview",
   data() {
-    const store      = useStore()
-    const auth       = computed(() => store.state.auth)
-    let lab          = ref({}) // 研究室名，コード
-    const reviews    = ref({}) // 投稿されているreviewの集合
-    const reviewBody = ref("") // reviewの文章
-    const replys     = ref([]) // 投稿されているreplyの集合
-    const replyBody  = ref([]) // replyの文章
+    const store           = useStore()
+    const auth            = computed(() => store.state.auth)
+    let lab               = ref({}) // 研究室名，コード
+    const reviews         = ref({}) // 投稿されているreviewの集合
+    const reviewBody      = ref("") // reviewの文章
+    const replys          = ref([]) // 投稿されているreplyの集合
+    const replyBody       = ref([]) // replyの文章
+    const reviewLgtm      = ref([]) // ユーザがreviewをLGTMしているかどうか
+    const reviewLgtmCount = ref([]) // reviewのLGTM数
 
     onMounted(async () => {
       try {
@@ -102,26 +112,45 @@ export default {
           "/lab/reviews/" + this.$route.params.professor
         )
         reviews.value = labReviewData.data
-
-        // 研究室レビューに対するリプライの取得
-        for (const d in labReviewData.data) {
-          const labReplyData = await axios.get(
-            "/lab/reply/" + labReviewData.data[d].ID
-          )
-          replys.value[labReviewData.data[d].ID] = labReplyData.data
+        for (const i in labReviewData.data) {
+          reviewLgtmCount.value[labReviewData.data[i].ID] = labReviewData.data[i].lgtm
         }
 
+        // 研究室レビューに対するリプライの取得
+        for (const i in labReviewData.data) {
+          const labReplyData = await axios.get(
+            "/lab/reply/" + labReviewData.data[i].ID
+          )
+          replys.value[labReviewData.data[i].ID] = labReplyData.data
+        }
+
+        // LGTM情報の取得
+        if (localStorage.isLogin) {
+          for (const i in labReviewData.data) {
+            // console.log(reviews.value[i])
+            const id = labReviewData.data[i].ID
+            const reviewLgtmData = await axios.get(
+              "/lgtm/lab/" + id + "/" + localStorage.userID
+            )
+            // ユーザはLGTMしているか?
+            if (reviewLgtmData.data.length == 0) {
+              reviewLgtm.value[id] = false
+            } else {
+              reviewLgtm.value[id] = true
+            }
+          }
+        }
 
         // lgtm情報の表示(lgtmボタン+件数)
         const lgtmInfos = document.getElementsByClassName('lgtm-info')
-        for(let i=0; i < lgtmInfos.length; i++){
+        for (const i in  lgtmInfos) {
           lgtmInfos[i].classList.remove("d-none")
         }
 
         // ログインしていない場合LGTMボタンをdisabledにする
-        if(!store.state.auth){
+        if (!store.state.auth) {
           const lgtmButtons = document.getElementsByClassName('lgtm')
-          for(let i=0; i < lgtmButtons.length; i++){
+          for (const i in lgtmButtons) {
             console.log(lgtmButtons[i])
             lgtmButtons[i].classList.add("disabled")
           }
@@ -130,6 +159,7 @@ export default {
         console.log(e)
       }
     })
+
     const submitReview = async () => {
       try {
         await axios.post("lab/review/post", {
@@ -164,6 +194,25 @@ export default {
       document.getElementById("reply-form-" + answerid).style.display = "block";
     }
 
+    const updateReviewLgtm = async (id) => {
+      // UIのLGTM数の変更
+      if (!reviewLgtm.value[id]) {
+        reviewLgtmCount.value[id]++
+      } else {
+        reviewLgtmCount.value[id]--
+      }
+      reviewLgtm.value[id] = !reviewLgtm.value[id]
+
+      try {
+        await axios.post("/lgtm/lab", {
+          lab_review_id : String(id),
+          user_id : localStorage.userID
+        })
+      } catch (e) {
+        console.log(e)
+      }
+    }
+
     return {
       auth,
       lab,
@@ -171,9 +220,12 @@ export default {
       replys,
       reviewBody,
       replyBody,
+      reviewLgtm,
+      reviewLgtmCount,
       submitReview,
       submitReply,
       displayReplyForm,
+      updateReviewLgtm,
     }
   }
 };
--- ui/src/pages/Question.vue
+++ ui/src/pages/Question.vue
@@ -11,7 +11,7 @@
       <span class="text-secondary m-0 pl-3">質問日時: {{ question.CreatedAt }} </span><br>
       <div class="d-none lgtm-info">
         <div v-if='!questionLgtm' class="mt-1">
-          <button @click="updateQuestionLgtm(false)" id="lgtm" class="btn btn-outline-primary lgtm">
+          <button @click="updateQuestionLgtm" id="lgtm" class="btn btn-outline-primary lgtm">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-hand-thumbs-up" viewBox="0 0 16 16">
               <path d="M8.864.046C7.908-.193 7.02.53 6.956 1.466c-.072 1.051-.23 2.016-.428 2.59-.125.36-.479 1.013-1.04 1.639-.557.623-1.282 1.178-2.131 1.41C2.685 7.288 2 7.87 2 8.72v4.001c0 .845.682 1.464 1.448 1.545 1.07.114 1.564.415 2.068.723l.048.03c.272.165.578.348.97.484.397.136.861.217 1.466.217h3.5c.937 0 1.599-.477 1.934-1.064a1.86 1.86 0 0 0 .254-.912c0-.152-.023-.312-.077-.464.201-.263.38-.578.488-.901.11-.33.172-.762.004-1.149.069-.13.12-.269.159-.403.077-.27.113-.568.113-.857 0-.288-.036-.585-.113-.856a2.144 2.144 0 0 0-.138-.362 1.9 1.9 0 0 0 .234-1.734c-.206-.592-.682-1.1-1.2-1.272-.847-.282-1.803-.276-2.516-.211a9.84 9.84 0 0 0-.443.05 9.365 9.365 0 0 0-.062-4.509A1.38 1.38 0 0 0 9.125.111L8.864.046zM11.5 14.721H8c-.51 0-.863-.069-1.14-.164-.281-.097-.506-.228-.776-.393l-.04-.024c-.555-.339-1.198-.731-2.49-.868-.333-.036-.554-.29-.554-.55V8.72c0-.254.226-.543.62-.65 1.095-.3 1.977-.996 2.614-1.708.635-.71 1.064-1.475 1.238-1.978.243-.7.407-1.768.482-2.85.025-.362.36-.594.667-.518l.262.066c.16.04.258.143.288.255a8.34 8.34 0 0 1-.145 4.725.5.5 0 0 0 .595.644l.003-.001.014-.003.058-.014a8.908 8.908 0 0 1 1.036-.157c.663-.06 1.457-.054 2.11.164.175.058.45.3.57.65.107.308.087.67-.266 1.022l-.353.353.353.354c.043.043.105.141.154.315.048.167.075.37.075.581 0 .212-.027.414-.075.582-.05.174-.111.272-.154.315l-.353.353.353.354c.047.047.109.177.005.488a2.224 2.224 0 0 1-.505.805l-.353.353.353.354c.006.005.041.05.041.17a.866.866 0 0 1-.121.416c-.165.288-.503.56-1.066.56z"/>
             </svg>
@@ -19,7 +19,7 @@
           <span class="pl-2">{{ question.lgtm }}件</span>
         </div>
         <div v-else class="mt-1">
-          <button @click="updateQuestionLgtm(true)" id="lgtm" class="btn btn-primary lgtm">
+          <button @click="updateQuestionLgtm" id="lgtm" class="btn btn-primary lgtm">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-hand-thumbs-up" viewBox="0 0 16 16">
               <path d="M8.864.046C7.908-.193 7.02.53 6.956 1.466c-.072 1.051-.23 2.016-.428 2.59-.125.36-.479 1.013-1.04 1.639-.557.623-1.282 1.178-2.131 1.41C2.685 7.288 2 7.87 2 8.72v4.001c0 .845.682 1.464 1.448 1.545 1.07.114 1.564.415 2.068.723l.048.03c.272.165.578.348.97.484.397.136.861.217 1.466.217h3.5c.937 0 1.599-.477 1.934-1.064a1.86 1.86 0 0 0 .254-.912c0-.152-.023-.312-.077-.464.201-.263.38-.578.488-.901.11-.33.172-.762.004-1.149.069-.13.12-.269.159-.403.077-.27.113-.568.113-.857 0-.288-.036-.585-.113-.856a2.144 2.144 0 0 0-.138-.362 1.9 1.9 0 0 0 .234-1.734c-.206-.592-.682-1.1-1.2-1.272-.847-.282-1.803-.276-2.516-.211a9.84 9.84 0 0 0-.443.05 9.365 9.365 0 0 0-.062-4.509A1.38 1.38 0 0 0 9.125.111L8.864.046zM11.5 14.721H8c-.51 0-.863-.069-1.14-.164-.281-.097-.506-.228-.776-.393l-.04-.024c-.555-.339-1.198-.731-2.49-.868-.333-.036-.554-.29-.554-.55V8.72c0-.254.226-.543.62-.65 1.095-.3 1.977-.996 2.614-1.708.635-.71 1.064-1.475 1.238-1.978.243-.7.407-1.768.482-2.85.025-.362.36-.594.667-.518l.262.066c.16.04.258.143.288.255a8.34 8.34 0 0 1-.145 4.725.5.5 0 0 0 .595.644l.003-.001.014-.003.058-.014a8.908 8.908 0 0 1 1.036-.157c.663-.06 1.457-.054 2.11.164.175.058.45.3.57.65.107.308.087.67-.266 1.022l-.353.353.353.354c.043.043.105.141.154.315.048.167.075.37.075.581 0 .212-.027.414-.075.582-.05.174-.111.272-.154.315l-.353.353.353.354c.047.047.109.177.005.488a2.224 2.224 0 0 1-.505.805l-.353.353.353.354c.006.005.041.05.041.17a.866.866 0 0 1-.121.416c-.165.288-.503.56-1.066.56z"/>
             </svg>
@@ -41,7 +41,7 @@
           <span class="text-secondary m-0 pl-3">回答日時: {{ answer.CreatedAt }} </span>
           <div class="d-none lgtn-info">
             <div v-if="!answerLgtm[answer.ID]" class="mt-1">
-              <button @click="updateAnswerLgtm(false, answer.ID)" id="lgtm" class="btn btn-outline-primary lgtm">
+              <button @click="updateAnswerLgtm(answer.ID)" id="lgtm" class="btn btn-outline-primary lgtm">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-hand-thumbs-up" viewBox="0 0 16 16">
                   <path d="M8.864.046C7.908-.193 7.02.53 6.956 1.466c-.072 1.051-.23 2.016-.428 2.59-.125.36-.479 1.013-1.04 1.639-.557.623-1.282 1.178-2.131 1.41C2.685 7.288 2 7.87 2 8.72v4.001c0 .845.682 1.464 1.448 1.545 1.07.114 1.564.415 2.068.723l.048.03c.272.165.578.348.97.484.397.136.861.217 1.466.217h3.5c.937 0 1.599-.477 1.934-1.064a1.86 1.86 0 0 0 .254-.912c0-.152-.023-.312-.077-.464.201-.263.38-.578.488-.901.11-.33.172-.762.004-1.149.069-.13.12-.269.159-.403.077-.27.113-.568.113-.857 0-.288-.036-.585-.113-.856a2.144 2.144 0 0 0-.138-.362 1.9 1.9 0 0 0 .234-1.734c-.206-.592-.682-1.1-1.2-1.272-.847-.282-1.803-.276-2.516-.211a9.84 9.84 0 0 0-.443.05 9.365 9.365 0 0 0-.062-4.509A1.38 1.38 0 0 0 9.125.111L8.864.046zM11.5 14.721H8c-.51 0-.863-.069-1.14-.164-.281-.097-.506-.228-.776-.393l-.04-.024c-.555-.339-1.198-.731-2.49-.868-.333-.036-.554-.29-.554-.55V8.72c0-.254.226-.543.62-.65 1.095-.3 1.977-.996 2.614-1.708.635-.71 1.064-1.475 1.238-1.978.243-.7.407-1.768.482-2.85.025-.362.36-.594.667-.518l.262.066c.16.04.258.143.288.255a8.34 8.34 0 0 1-.145 4.725.5.5 0 0 0 .595.644l.003-.001.014-.003.058-.014a8.908 8.908 0 0 1 1.036-.157c.663-.06 1.457-.054 2.11.164.175.058.45.3.57.65.107.308.087.67-.266 1.022l-.353.353.353.354c.043.043.105.141.154.315.048.167.075.37.075.581 0 .212-.027.414-.075.582-.05.174-.111.272-.154.315l-.353.353.353.354c.047.047.109.177.005.488a2.224 2.224 0 0 1-.505.805l-.353.353.353.354c.006.005.041.05.041.17a.866.866 0 0 1-.121.416c-.165.288-.503.56-1.066.56z"/>
                 </svg>
@@ -49,7 +49,7 @@
               <span class="pl-2">{{ answerLgtmCount[answer.ID] }}件</span>
             </div>
             <div v-else class="mt-1">
-              <button @click="updateAnswerLgtm(true, answer.ID)" id="lgtm" class="btn btn-primary lgtm">
+              <button @click="updateAnswerLgtm(answer.ID)" id="lgtm" class="btn btn-primary lgtm">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-hand-thumbs-up" viewBox="0 0 16 16">
                   <path d="M8.864.046C7.908-.193 7.02.53 6.956 1.466c-.072 1.051-.23 2.016-.428 2.59-.125.36-.479 1.013-1.04 1.639-.557.623-1.282 1.178-2.131 1.41C2.685 7.288 2 7.87 2 8.72v4.001c0 .845.682 1.464 1.448 1.545 1.07.114 1.564.415 2.068.723l.048.03c.272.165.578.348.97.484.397.136.861.217 1.466.217h3.5c.937 0 1.599-.477 1.934-1.064a1.86 1.86 0 0 0 .254-.912c0-.152-.023-.312-.077-.464.201-.263.38-.578.488-.901.11-.33.172-.762.004-1.149.069-.13.12-.269.159-.403.077-.27.113-.568.113-.857 0-.288-.036-.585-.113-.856a2.144 2.144 0 0 0-.138-.362 1.9 1.9 0 0 0 .234-1.734c-.206-.592-.682-1.1-1.2-1.272-.847-.282-1.803-.276-2.516-.211a9.84 9.84 0 0 0-.443.05 9.365 9.365 0 0 0-.062-4.509A1.38 1.38 0 0 0 9.125.111L8.864.046zM11.5 14.721H8c-.51 0-.863-.069-1.14-.164-.281-.097-.506-.228-.776-.393l-.04-.024c-.555-.339-1.198-.731-2.49-.868-.333-.036-.554-.29-.554-.55V8.72c0-.254.226-.543.62-.65 1.095-.3 1.977-.996 2.614-1.708.635-.71 1.064-1.475 1.238-1.978.243-.7.407-1.768.482-2.85.025-.362.36-.594.667-.518l.262.066c.16.04.258.143.288.255a8.34 8.34 0 0 1-.145 4.725.5.5 0 0 0 .595.644l.003-.001.014-.003.058-.014a8.908 8.908 0 0 1 1.036-.157c.663-.06 1.457-.054 2.11.164.175.058.45.3.57.65.107.308.087.67-.266 1.022l-.353.353.353.354c.043.043.105.141.154.315.048.167.075.37.075.581 0 .212-.027.414-.075.582-.05.174-.111.272-.154.315l-.353.353.353.354c.047.047.109.177.005.488a2.224 2.224 0 0 1-.505.805l-.353.353.353.354c.006.005.041.05.041.17a.866.866 0 0 1-.121.416c-.165.288-.503.56-1.066.56z"/>
                 </svg>
@@ -122,11 +122,11 @@ export default {
     const question        = ref({}) // questionの内容
     const answers         = ref({}) // 投稿されているanswerの集合
     const answerBody      = ref("") // 投稿時のanswerの文章
-    const answerLgtmCount = ref([]) // answerのLGTM数
     const replys          = ref({}) // 投稿されているreplyの集合
     const replyBody       = ref([]) // 投稿時のreplyの文章
     const questionLgtm    = ref()   // ユーザがquestionをLGTMしているかどうか
     const answerLgtm      = ref([]) // ユーザがquestionをLGTMしているかどうか
+    const answerLgtmCount = ref([]) // answerのLGTM数
 
     onMounted(async () => {
       try {
@@ -164,8 +164,8 @@ export default {
           }
 
           // answerに対してユーザがLGTMしているか?
-          for (const d in answerData.data) {
-            const id = answerData.data[d].ID
+          for (const i in answerData.data) {
+            const id = answerData.data[i].ID
             const answerLgtmData = await axios.get(
               "/lgtm/answer/" + id + "/" + localStorage.userID
             )
@@ -179,13 +179,19 @@ export default {
 
         // lgtm情報の表示(lgtmボタン+件数)
         const lgtmInfos = document.getElementsByClassName('lgtm-info')
+        // for (const i in lgtmInfos) {
+        //   lgtmInfos[i].classList.remove("d-none")
+        // }
         for(let i=0; i < lgtmInfos.length; i++){
           lgtmInfos[i].classList.remove("d-none")
         }
 
         // ログインしていない場合LGTMボタンをdisabledにする
-        if(!store.state.auth){
+        if (!store.state.auth) {
           const lgtmButtons = document.getElementsByClassName('lgtm')
+          // for (const i in lgtmButtons) {
+          //   lgtmButtons[i].classList.add("disabled")
+          // }
           for(let i=0; i < lgtmButtons.length; i++){
             console.log(lgtmButtons[i])
             lgtmButtons[i].classList.add("disabled")
@@ -227,13 +233,14 @@ export default {
 
     // questionのLGTMボタンが押された時
     // TODO destroy時までLGTM情報を持っておいて，一度に送信させる
-    const updateQuestionLgtm = async (lgtmed) => {
-      questionLgtm.value = !questionLgtm.value
-      if (!lgtmed) {
+    const updateQuestionLgtm = async () => {
+      if (!questionLgtm.value) {
         question.value.lgtm++
       } else {
         question.value.lgtm--
       }
+      questionLgtm.value = !questionLgtm.value
+
       try {
         await axios.post("/lgtm/question", {
           question_id : this.$route.params.question_id,
@@ -245,13 +252,15 @@ export default {
     }
 
     // answerのLGTMボタンが押された時
-    const updateAnswerLgtm = async (lgtmed, id) => {
-      answerLgtm.value[id] = !answerLgtm.value[id]
-      if (!lgtmed) {
+    const updateAnswerLgtm = async (id) => {
+      // UIのLGTM数の変更
+      if (!answerLgtm.value[id]) {
         answerLgtmCount.value[id]++
       } else {
         answerLgtmCount.value[id]--
       }
+      answerLgtm.value[id] = !answerLgtm.value[id]
+
       try {
         await axios.post("/lgtm/answer", {
           answer_id : String(id),
@@ -272,12 +281,12 @@ export default {
       auth,
       question,
       answers,
-      answerLgtmCount,
       replys,
       answerBody,
       replyBody,
       questionLgtm,
       answerLgtm,
+      answerLgtmCount,
       submitAnswer,
       submitReply,
       updateQuestionLgtm,
