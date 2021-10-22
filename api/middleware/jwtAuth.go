f4d62e21c54a6bd9f38887e6446e33f0deed7e69
kenkn <skt7tp@gmail.com>
2021-10-22 19:03:35 +0900

    コメント文の追加

--- api/middleware/jwtAuth.go
+++ api/middleware/jwtAuth.go
@@ -9,6 +9,13 @@ type Claims struct {
 	jwt.StandardClaims
 }
 
+// tryAuth(private)
+// 機能 : JWTトークンを用いた認証
+// 引数 :
+//  * tokenString : JWTトークン
+// 戻り値 :
+// 	* *jwt.Token : JWTトークンで得られたclaim
+//  * bool       : 認証成功ならばtrue
 func tryAuth(tokenString string) (*jwt.Token, bool) {
 	// JWTtoken取得
 	token, err := jwt.ParseWithClaims(tokenString, &Claims{}, func(token *jwt.Token) (interface{}, error) {
@@ -22,10 +29,22 @@ func tryAuth(tokenString string) (*jwt.Token, bool) {
 	return token, true
 }
 
+// GetAuthToken(public)
+// 機能 : JWTトークンで得られたclaimの取得
+// 引数 :
+//  * tokenString : JWTトークン
+// 戻り値 :
+// 	* *jwt.Token : JWTトークンで得られたclaim
+//  * bool       : 認証成功ならばtrue
 func GetAuthToken(tokenString string) (*jwt.Token, bool) {
 	return tryAuth(tokenString)
 }
 
+// CanAuth(public)
+// 機能 : JWT認証ができたかどうかの判定
+// 引数 :
+//  * tokenString : JWTトークン
+// 戻り値 : 認証成功ならばtrue
 func CanAuth(tokenString string) bool {
 	_, canAuth := tryAuth(tokenString)
 	return canAuth
