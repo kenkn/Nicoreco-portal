3b185012a046cd7d1defc4524f011c5439942ca4
Furushiima <hideharu.furushima@gmail.com>
2022-01-13 11:14:13 +0900

    Add subjectButton with logo

---
+++ ui/src/components/SubjectButton.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class='card m-2' style="width: 14rem;">
+      <img :src="subjectLogo" alt="subject-logo" class="bd-placeholder-img card-img-top border-bottom" width="100%" style="height: 12rem;">
+    <div class="card-body">
+      <h5 class="card-title text-center">{{ subjectName }}</h5>
+      <router-link v-bind:to="'/question/' + subjectCode" class="stretched-link"></router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref, onMounted } from 'vue'
+
+export default {
+  name: "SubjectButton",
+  props: {
+    subjectName: {
+      type: String,
+      require: true,
+    },
+    subjectCode: {
+      type: String,
+      require: true,
+    },
+  },
+  setup(props) {
+    const subjectLogo = ref()
+    subjectLogo.value = require('@/assets/img/subject_icons/' + props.subjectCode + '.png')
+    return {
+      subjectLogo,
+    }
+
+  }
+}
+</script>
+
+<style scoped>
+  /* 共通 */
+  .stretched-link:hover::after {
+    background: black;
+    opacity: 0.1;
+  }
+  /* スマホ版 */
+  .card {
+    margin-right: auto;
+    margin-left: auto;
+  }
+  /* PC版 */
+  /*画面サイズが768px(BootstrapのMedium)からはここを読み込む*/
+  @media screen and (min-width:768px) {
+    .card {
+    }
+  }
+</style>
--- ui/src/pages/Subjects.vue
+++ ui/src/pages/Subjects.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="container">
     <h1 class="pb-3 display-5">講義一覧</h1>
-  <div class="row">
-    <router-link v-for="subject in subjects" :key="subject.name" v-bind:to="'/question/' + subject.code" tag="li" class="btn btn-link col col-12 col-md-3 col-lg-2 m-3 p-0 text-center text-dark rounded shadow-sm link-box">
-      <p class="text-center fw-bolder p-3 mt-0 my-2 w-100 h-100">{{ subject.name }}</p>
-    </router-link>
-  </div>
+    <div class="row">
+      <SubjectButton 
+        v-for="subject in subjects" 
+        :key="subject.name"
+        :subject-name="subject.name" 
+        :subject-code="subject.code"
+      />
+    </div>
   </div>
 </template>
  
 <script>
 import data from '../data/subject-data.json'
+import SubjectButton from '@/components/SubjectButton'
+
 export default {
   name: "Subjects",
+  components: {
+    SubjectButton
+  },
   setup() {
     return {
       subjects: data
@@ -20,13 +28,3 @@ export default {
   },
 };
 </script>
-<style scoped>
-  .link-box {
-    border-width: 1px;
-    border-color: rgb(167, 167, 167);
-    background-color: white;
-  }
-  .link-box :hover{
-    background: rgb(230, 229, 227);
-  }
-</style>
