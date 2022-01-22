d081773de0681bcfc39f6b399879335ba7cd54ac
Furushiima <hideharu.furushima@gmail.com>
2022-01-22 18:55:06 +0900

    Add jumbo to store

--- ui/src/components/CodeBar.vue
+++ ui/src/components/CodeBar.vue
@@ -1,8 +1,8 @@
 <template>
   <div class=position-relative>
     <img :src="codeImg" height="200" alt="code-bar" class="w-100 p-1">
-    <div class="position-absolute top-0 start-0 text-light fw-bold p-3"> 
-        {{ message }}
+    <div class="position-absolute top-0 start-0 text-light fw-bold display-6 p-5 pt-4" style="width: 65%"> 
+        {{ jumbotron }}
     </div>
     <div class="position-absolute top-0 end-0 text-light text-center p-5">
         <h3><em>Nicoreco Portal</em></h3>
@@ -15,7 +15,6 @@
 <script>
 import { computed } from 'vue'
 import { useStore } from 'vuex'
-import UserIconMenu from "@/components/UserIconMenu"
 import codeImg from '@/assets/img/code.png'
 
 export default {
@@ -23,9 +22,9 @@ export default {
   setup() {
     const store = useStore()
     const auth = computed(() => store.state.auth)
-    const message = "講義一覧"
+    const jumbotron = computed(() => store.state.jumbotron)
     return {
-      message,
+      jumbotron,
       codeImg,
     }
   }
