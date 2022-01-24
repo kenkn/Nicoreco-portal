47d38b0f3548c7a7ce00c6a0d83abffec43e5f14
kenkn <skt7tp@gmail.com>
2022-01-24 19:32:28 +0900

    improve code snippet

--- ui/src/pages/Question.vue
+++ ui/src/pages/Question.vue
@@ -10,13 +10,13 @@
         
         <div v-for="(qBody, idx) in questionBodies" :key="idx">
           <p>{{ qBody }}</p>
-          <p v-html="questionCodeBodies[idx]"/>
+          <pre v-html="questionCodeBodies[idx]"/>
         </div>
 
         <!-- デバッグ用 TODO 消す -->
         <span class="text-secondary m-0">ID: {{ question.ID }} </span>
         <span class="text-secondary m-0">質問者: {{ question.questioner_id }} </span>
-        <span class="text-secondary m-0 pl-3">質問日時: {{ FormatDate(question.CreatedAt) }} </span><br>
+        <span class="text-secondary m-0 pl-3">質問日時: {{ question.CreatedAt }} </span><br>
         <div v-if='!questionLgtm' class="mt-1">
           <button @click="updateQuestionLgtm" id="lgtm" class="btn btn-outline-primary lgtm">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-hand-thumbs-up" viewBox="0 0 16 16">
@@ -39,15 +39,21 @@
       <div class="mt-5 border border-dark bg-white rounded">
         <h1 class="p-4 display-6 border-bottom border-dark">{{ answers.length }}件の回答</h1>
   
-        <div v-for="(answer, idx) in answers" :key="answer.answer.ID" class="border-bottom border-dark p-4 mt-2">
+        <div v-for="(answer, idx) in answers" :key="idx" class="border-bottom border-dark p-4 mt-2">
+          <!-- <h1>{{answer}}</h1> -->
           <div class="border p-3 mb-2 shadow-sm">
-            <h4>{{ answer.answer.body }}</h4>
+            <!-- <h4>{{ answer.answer.body }}</h4> -->
+
+            <div v-for="(aBody, aidx) in answerBodies[idx]" :key="aidx">
+              <p>{{ answerBodies[idx][aidx] }}</p>
+              <pre v-html="answerCodeBodies[idx][aidx]"/>
+            </div>
             <!-- デバッグ用 TODO 消す -->
-            <span class="text-secondary m-0">ID: {{ answer.ID }} </span>
-            <span class="text-secondary m-0">回答者: {{ answer.user_id }} </span>
-            <span class="text-secondary m-0 pl-3">回答日時: {{ FormatDate(answer.CreatedAt) }} </span>
-            <div v-if="!answerLgtm[answer.ID]" class="mt-1">
-              <button @click="updateAnswerLgtm(answer.ID)" id="lgtm" class="btn btn-outline-primary lgtm">
+            <span class="text-secondary m-0">ID: {{ answer.answer.ID }} </span>
+            <span class="text-secondary m-0">回答者: {{ answer.answer.answerer_id }} </span>
+            <span class="text-secondary m-0 pl-3">回答日時: {{ answer.answer.CreatedAt }} </span>
+            <div v-if="!answer.islgtmed" class="mt-1">
+              <button @click="updateAnswerLgtm(idx)" id="lgtm" class="btn btn-outline-primary lgtm">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-hand-thumbs-up" viewBox="0 0 16 16">
                   <path d="M8.864.046C7.908-.193 7.02.53 6.956 1.466c-.072 1.051-.23 2.016-.428 2.59-.125.36-.479 1.013-1.04 1.639-.557.623-1.282 1.178-2.131 1.41C2.685 7.288 2 7.87 2 8.72v4.001c0 .845.682 1.464 1.448 1.545 1.07.114 1.564.415 2.068.723l.048.03c.272.165.578.348.97.484.397.136.861.217 1.466.217h3.5c.937 0 1.599-.477 1.934-1.064a1.86 1.86 0 0 0 .254-.912c0-.152-.023-.312-.077-.464.201-.263.38-.578.488-.901.11-.33.172-.762.004-1.149.069-.13.12-.269.159-.403.077-.27.113-.568.113-.857 0-.288-.036-.585-.113-.856a2.144 2.144 0 0 0-.138-.362 1.9 1.9 0 0 0 .234-1.734c-.206-.592-.682-1.1-1.2-1.272-.847-.282-1.803-.276-2.516-.211a9.84 9.84 0 0 0-.443.05 9.365 9.365 0 0 0-.062-4.509A1.38 1.38 0 0 0 9.125.111L8.864.046zM11.5 14.721H8c-.51 0-.863-.069-1.14-.164-.281-.097-.506-.228-.776-.393l-.04-.024c-.555-.339-1.198-.731-2.49-.868-.333-.036-.554-.29-.554-.55V8.72c0-.254.226-.543.62-.65 1.095-.3 1.977-.996 2.614-1.708.635-.71 1.064-1.475 1.238-1.978.243-.7.407-1.768.482-2.85.025-.362.36-.594.667-.518l.262.066c.16.04.258.143.288.255a8.34 8.34 0 0 1-.145 4.725.5.5 0 0 0 .595.644l.003-.001.014-.003.058-.014a8.908 8.908 0 0 1 1.036-.157c.663-.06 1.457-.054 2.11.164.175.058.45.3.57.65.107.308.087.67-.266 1.022l-.353.353.353.354c.043.043.105.141.154.315.048.167.075.37.075.581 0 .212-.027.414-.075.582-.05.174-.111.272-.154.315l-.353.353.353.354c.047.047.109.177.005.488a2.224 2.224 0 0 1-.505.805l-.353.353.353.354c.006.005.041.05.041.17a.866.866 0 0 1-.121.416c-.165.288-.503.56-1.066.56z"/>
                 </svg>
@@ -64,15 +70,15 @@
             </div>
           </div>
   
-          <template v-for="reply in replys" :key="reply.ID">
-            <div v-if="reply.parent_id==answer.ID">
+          <template v-for="reply in answer.reply" :key="reply.ID">
+            <div>
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots-vertical  my-2 ml-5" viewBox="0 0 16 16">
                 <path d="M5.921 11.9 1.353 8.62a.719.719 0 0 1 0-1.238L5.921 4.1A.716.716 0 0 1 7 4.719V6c1.5 0 6 0 7 8-2.5-4.5-7-4-7-4v1.281c0 .56-.606.898-1.079.62z"/>
               </svg>
               <div class="border p-2 ml-5 mb-2 shadow-sm">
                 <p>{{ reply.body }}</p>
-                <span class="text-secondary m-0">返信者: {{ reply.user_id }} </span>
-                <span class="text-secondary m-0 pl-3">返信日時: {{ FormatDate(reply.CreatedAt) }} </span><br>
+                <span class="text-secondary m-0">返信者: {{ reply.replyer_id }} </span>
+                <span class="text-secondary m-0 pl-3">返信日時: {{ reply.CreatedAt }} </span><br>
               </div>
             </div>
           </template>
@@ -104,7 +110,7 @@
               ログインして回答を送信
             </router-link>
             <div v-else class="form-group">
-              <textarea v-model="answerBody" class="form-control p-1 my-2 mb-4 shadow-sm" rows="4" placeholder='回答を追加' required />
+              <textarea v-model="postAnswerBody" class="form-control p-1 my-2 mb-4 shadow-sm" rows="4" placeholder='回答を追加' required />
               <button class="btn btn-outline-primary w-100" type="submit">
                 回答を送信
               </button>
@@ -122,7 +128,7 @@ import axios from 'axios'
 import { useRoute, useRouter } from 'vue-router'
 import { useStore } from 'vuex'
 import Loader from "@/components/Loader"
-import FormatDate from '../functions/FormatDate'
+import FormatDate from '@/functions/FormatDate'
 import hljs from 'highlight.js';
 import 'highlight.js/styles/xcode.css';
 
@@ -132,19 +138,21 @@ export default {
     Loader
   },
   setup() {
-    const store           = useStore()
-    const route           = useRoute()
-    const router          = useRouter()
-    const auth            = computed(() => store.state.auth)
-    const question        = ref({}) // questionの内容
+    const store              = useStore()
+    const route              = useRoute()
+    const router             = useRouter()
+    const auth               = computed(() => store.state.auth)
+    const question           = ref({}) // questionの内容
     const questionBodies     = ref([])
     const questionCodeBodies = ref([])
-    const answers         = ref([]) // 投稿されているanswerの集合
-    const answerBody      = ref("") // 投稿時のanswerの文章
-    const replys          = ref({}) // 投稿されているreplyの集合
-    const replyBody       = ref([]) // 投稿時のreplyの文章
-    const questionLgtm    = ref()   // ユーザがquestionをLGTMしているかどうか
-    const loading         = ref(true) // ロード中であるか(mountedの最後にロード画面を解除)
+    const answers            = ref([]) // 投稿されているanswerの集合
+    const answerBodies       = ref([])
+    const answerCodeBodies   = ref([])
+    const postAnswerBody     = ref("") // 投稿時のanswerの文章
+    const replys             = ref({}) // 投稿されているreplyの集合
+    const replyBody          = ref([]) // 投稿時のreplyの文章
+    const questionLgtm       = ref()   // ユーザがquestionをLGTMしているかどうか
+    const loading            = ref(true) // ロード中であるか(mountedの最後にロード画面を解除)
 
     onMounted(async () => {
       try {
@@ -172,23 +180,59 @@ export default {
       } catch (e) {
         console.log(e)
       }
-  
 
-      const splitedBody = question.value.body.split('```')
-      for (const i in splitedBody) {
-        if (i%2) {
+      const splitedQuestionBody = question.value.body.split('```')
+      for (const i in splitedQuestionBody) {
+        if (i % 2 == 1) {
           // const re = /(?<=\n)([\s\S]*)(?=\n)/
           // TODO
           // preタグで表示させたい〜〜〜！(現状だと改行がキモい)
-          const code = splitedBody[i]
+          let sliceIndex = 0
+          for (let cidx = 0; cidx < splitedQuestionBody[i].length; cidx++) {
+            // console.log(splitedAnswerBody[i][cidx])
+            if (splitedQuestionBody[i][cidx] == ' ') {
+              continue
+            } else if (splitedQuestionBody[i][cidx] == '\n') {
+              sliceIndex = cidx + 1
+              break
+            } else {
+              break
+            }
+          }
+          const code = splitedQuestionBody[i].slice(sliceIndex)
           const highlightedCode = '<p style="background-color: #eee">' + hljs.highlightAuto(code).value + '</p>'
           questionCodeBodies.value.push(highlightedCode)
         } else {
-          questionBodies.value.push(splitedBody[i])
+          questionBodies.value.push(splitedQuestionBody[i])
         }
       }
-      if (questionBodies.value.length > questionCodeBodies.value.length) {
-        questionCodeBodies.value.push('')
+
+      for (const i in answers.value) {
+        let aBodies = []
+        let aCode = []
+        const splitedAnswerBody = answers.value[i].answer.body.split('```')
+        for (const j in splitedAnswerBody) {
+          if (j % 2 == 1) {
+            let sliceIndex = 0
+            for (let cidx = 0; cidx < splitedAnswerBody[j].length; cidx++) {
+              if (splitedAnswerBody[j][cidx] == ' ') {
+                continue
+              } else if (splitedAnswerBody[j][cidx] == '\n') {
+                sliceIndex = cidx + 1
+                break
+              } else {
+                break
+              }
+            }
+            const code = splitedAnswerBody[j].slice(sliceIndex)
+            const highlightedCode = '<p style="background-color: #eee">' + hljs.highlightAuto(code).value + '</p>'
+            aCode.push(highlightedCode)
+          } else {
+            aBodies.push(splitedAnswerBody[j])
+          }
+        }
+        answerBodies.value.push(aBodies)
+        answerCodeBodies.value.push(aCode)
       }
 
       // ロード画面を解除
@@ -200,7 +244,7 @@ export default {
         await axios.post("answer/post", {
           parent_id : route.params.question_id,
           user_id : localStorage.userID,
-          body : answerBody.value
+          body : postAnswerBody.value
         })
         // リロード
         router.go({path: router.currentRoute.path, force: true})
@@ -271,8 +315,10 @@ export default {
       questionBodies,
       questionCodeBodies,
       answers,
+      answerBodies,
+      answerCodeBodies,
       replys,
-      answerBody,
+      postAnswerBody,
       replyBody,
       questionLgtm,
       loading,
