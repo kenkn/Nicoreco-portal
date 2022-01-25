bf767f3b62764926ceee90a5f8b2af7e7f4b2520
kenkn <skt7tp@gmail.com>
2022-01-25 21:52:06 +0900

    コードハイライトの不具合修正(正規表現による実現)

--- ui/src/functions/extractBodyCode.js
+++ ui/src/functions/extractBodyCode.js
@@ -3,23 +3,21 @@ import 'highlight.js/styles/xcode.css';
 import getCodeContent from "./getCodeContent"
 
 // extractBodyCode
-// 機能 : コードハイライト用の
-// HACK 
-//   * エスケープ文字によるコード回避，正規表現によるコード部分抽出
-// "```" でbodyを分割すると，偶数idxの時は非コード部分，奇数idxの時はコード部分と考えることができる．
+// 機能 : コードハイライトの作成
 export default function extractBodyCode(content) {
     let bodies = []
     let codes = []
-    const splitedContent = content.split('```')
-    for (let i = 0; i < splitedContent.length; ++i) {
-        if (i % 2 == 1) {
-            const code = getCodeContent(splitedContent[i])
-            const highlightedCode = '<p style="background-color: #eee">' + hljs.highlightAuto(code).value + '</p>'
-            codes.push(highlightedCode)
-        } else {
-            bodies.push(getCodeContent(splitedContent[i]))
-        }
+    const re = /```[\s\S]*?```/g
+    let sliceStartIdx = 0
+    for (let c of content.matchAll(re)) {
+        const body = content.slice(sliceStartIdx, sliceStartIdx + c.index - 1)
+        const code = getCodeContent(c[0].slice(3, -3))
+        const highlightedCode = '<p style="background-color: #eee">' + hljs.highlightAuto(code).value + '</p>'
+        bodies.push(body)
+        codes.push(highlightedCode)
+        sliceStartIdx = c.index + c[0].length
     }
+    bodies.push(content.slice(sliceStartIdx))
 
     return [bodies, codes]
 }
