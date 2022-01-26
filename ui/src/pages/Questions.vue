5f945efbbf33acbcd54d8d8deae56b8abc936162
Furushiima <hideharu.furushima@gmail.com>
2022-01-27 00:33:36 +0900

    Add ellipsis to Labreviews page and Questions pages

--- ui/src/pages/Questions.vue
+++ ui/src/pages/Questions.vue
@@ -27,7 +27,7 @@
         <!-- list -->
         <div class="list-group">
           <router-link v-for="question in questions" :key="question.ID" v-bind:to="$route.path  + '/' + question.ID" class="list-group-item list-group-item-action p-4">
-            <div class="fw-bold text-break pr-3 pb-4">
+            <div class="fw-bold mr-3 mb-3" style="display: -webkit-box; -webkit-line-clamp: 3; -webkit-box-orient: vertical; overflow: hidden; text-overflow: ellipsis;">
               {{ question.title }}
             </div>
             <span class="badge bg-light text-dark text-muted mr-1">
