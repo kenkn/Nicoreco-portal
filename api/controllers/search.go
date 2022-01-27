62c20cf26af85d145ad6a87f7013c1203b6ae7e1
kenkn <skt7tp@gmail.com>
2022-01-27 22:27:27 +0900

    検索機能の修正

--- api/controllers/search.go
+++ api/controllers/search.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"auth-api/database"
 	"auth-api/models"
+	"net/url"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -10,11 +11,11 @@ import (
 func Search(c *fiber.Ctx) error {
 
 	searchRawBody := c.Params("+")
+	searchRawBody, _ = url.QueryUnescape(searchRawBody)
 
 	searchResult := []models.Question{}
-
 	database.DB.Raw("SELECT * FROM questions WHERE MATCH(title, body) AGAINST(? IN NATURAL LANGUAGE MODE)", searchRawBody).Scan(&searchResult)
-
-	return c.JSON(searchResult)
 	
+	return c.JSON(searchResult)
+
 }
